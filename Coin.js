66ba831e18b2a471e3a828b81203b6cd1ee840cc
Ali Pirsadeghi <99328659+Allipsiss@users.noreply.github.com>
2025-02-14 13:43:48 +0330

    heyyyy

--- Coin.js
+++ Coin.js
@@ -1,9 +1,6 @@
 import fetch from "node-fetch";
 import axios from "axios";
 
-const botToken = process.env.TELEGRAM_BOT_TOKEN;  // Access the secret from the environment
-const chatId = process.env.TELEGRAM_CHAT_ID;      // Access the secret from the environment
-
 async function getCommodityPrices() {
     try {
         const response = await fetch("https://call1.tgju.org/ajax.json");
@@ -51,8 +48,8 @@ async function getCommodityPrices() {
 }
 
 async function sendMessageToTelegram(message) {
-    const botToken = TELEGRAM_BOT_TOKEN;  // Replace with your actual bot token
-    const chatId = TELEGRAM_CHAT_ID;      // Replace with your actual chat ID
+    const botToken = process.env.TELEGRAM_BOT_TOKEN;  // Replace with your actual bot token
+    const chatId = process.env.TELEGRAM_CHAT_ID;      // Replace with your actual chat ID
 
     const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
     const params = {
